6454ca477c4770c6d534f51d7ed54420c6801b08
Greengrass-Game-coder <mohymiber@gmail.com>
2026-07-27 03:12:40 +0300

    Add WebSocket server to handle client connections

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
 
+console.log('Master server running on port 8080');
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('message', (message) => {
+    console.log('Received:', message);
+  });
+});
